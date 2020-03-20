6dbd0d2eca5d587802c9efff6a2ec76229eedba8
徐京徽 <574824551@qq.com>
2020-03-20 16:33:13 +0800

    Update 最长公共前缀.py

--- 最长公共前缀.py
+++ 最长公共前缀.py
@@ -11,3 +11,15 @@ class Solution(object):
             if x != s2[i]:
                 return s2[:i]
         return s1
+        #zip压缩法
+        '''
+        if not strs: return ""
+        ss = list(map(set, zip(*strs)))
+        res = ""
+        for i, x in enumerate(ss):
+            x = list(x)
+            if len(x) > 1:
+                break
+            res = res + x[0]
+        return res
+        '''
