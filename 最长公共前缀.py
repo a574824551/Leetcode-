4576760e95edf7e06e8eccffd2301716bf7e9814
徐京徽 <574824551@qq.com>
2020-03-20 16:25:22 +0800

    Create 最长公共前缀.py
    引用了评论区xshura的做法，觉得确实很精妙，以下为原评论：python两种让你拍大腿的解法，时间复杂度你想象不到，短小精悍。 1、利用python的max()和min()，在Python里字符串是可以比较的，按照ascII值排，举例abb， aba，abac，最大为abb，最小为aba。所以只需要比较最大最小的公共前缀就是整个数组的公共前缀

---
+++ 最长公共前缀.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i,x in enumerate(s1):
+            if x != s2[i]:
+                return s2[:i]
+        return s1
