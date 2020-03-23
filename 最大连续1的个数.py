cb626835e6e168d580ac5480d9018e615b147ed9
徐京徽 <574824551@qq.com>
2020-03-23 22:49:44 +0800

    Update 最大连续1的个数.py

--- 最大连续1的个数.py
+++ 最大连续1的个数.py
@@ -5,3 +5,4 @@ class Solution(object):
         :rtype: int
         """
         return max(len (substr) for substr in ''.join([str(x) for x in nums]).split("0"))
+        #转为字符串，以0为分隔符得到list，求元素中最长长度。
