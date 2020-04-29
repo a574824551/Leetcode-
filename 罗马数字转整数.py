76fd22522daab2467fbf98392a94fa1e8f011087
徐京徽 <574824551@qq.com>
2020-04-29 19:04:13 +0800

    Update 罗马数字转整数.py

--- 罗马数字转整数.py
+++ 罗马数字转整数.py
@@ -6,3 +6,14 @@ class Solution(object):
         """
         d = {'I':1, 'IV':3, 'V':5, 'IX':8, 'X':10, 'XL':30, 'L':50, 'XC':80, 'C':100, 'CD':300, 'D':500, 'CM':800, 'M':1000}
         return sum(d.get(s[max(i-1, 0):i+1], d[n]) for i, n in enumerate(s))
+    
+        '''
+        a = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}        
+        ans=0        
+        for i in range(len(s)):            
+            if i<len(s)-1 and a[s[i]]<a[s[i+1]]:                
+                ans-=a[s[i]]
+            else:
+                ans+=a[s[i]]
+        return ans
+        '''
