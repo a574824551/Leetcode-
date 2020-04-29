c549fdeb514e706144575398b34c178799b05fe8
徐京徽 <574824551@qq.com>
2020-04-29 19:09:31 +0800

    Create 移动零.py

---
+++ 移动零.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i]==0:
+                del(nums[i])
+                nums.append(0)
