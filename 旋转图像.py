b75f6db1157c6db17c0463cc7ebd2e0a05bdf9a4
徐京徽 <574824551@qq.com>
2020-05-19 16:07:49 +0800

    Create 旋转图像.py

---
+++ 旋转图像.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
+        #一次循环解决，把旋转的4个坐标计算出来，对4个元素进行替换既可。
+        n = len(matrix[0])        
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+                
+        #好理解的想法，先转置再镜像
+        n = len(matrix[0])        
+        # transpose matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i] 
+        
+        # reverse each row
+        for i in range(n):
+            matrix[i].reverse()
