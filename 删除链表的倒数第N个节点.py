112bf98f235bb2e7081550c515179026c92629cc
徐京徽 <574824551@qq.com>
2020-06-14 17:48:19 +0800

    Create 删除链表的倒数第N个节点.py

---
+++ 删除链表的倒数第N个节点.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        
+        a=head
+        b=head
+        
+        for i in range(n):
+            if a.next:
+                a = a.next
+            else:
+                return head.next
+        
+        while a.next:
+            a=a.next
+            b=b.next
+        
+        b.next = b.next.next
+        
+        return head
